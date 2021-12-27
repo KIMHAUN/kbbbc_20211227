2d0ccbacf2b5fe7fd1ffc7ed2db7859a3e4b81c7
kimhaun <wha02068@gmail.com>
2021-12-27 22:51:14 +0900

    배너 자동 롤링 이벤트 ><

--- app/src/main/java/fragments/HomeFragment.kt
+++ app/src/main/java/fragments/HomeFragment.kt
@@ -2,6 +2,8 @@ package fragments
 
 import adapters.ViewPagerAdapter
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,13 +17,21 @@ import kr.co.haun.kbbbc_20211227.R
 
 class HomeFragment: Fragment() {
 
+    private val MIN_SCALE = 0.85f // 뷰가 몇퍼센트로 줄어들 것인지
+    private val MIN_ALPHA = 0.5f // 어두워지는 정도를 나타낸 듯 하다.
 
+    var currentPosition=0
+
+    //핸들러 설정
+    //ui 변경하기
+    val handler= Handler(Looper.getMainLooper()){
+        setPage()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.fragment_home)
-
-
     }
 
     override fun onCreateView(
@@ -37,10 +47,73 @@ class HomeFragment: Fragment() {
 
         banner.adapter = ViewPagerAdapter(getBannerList()) // 어댑터 생성
         banner.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
+
+        //뷰페이저 넘기는 쓰레드
+        val thread=Thread(PagerRunnable())
+        thread.start()
+        
+        spring_dots_indicator.setViewPager2(banner) //indicator 설정
+
+    }
+
+    //페이지 변경하기
+    fun setPage(){
+        if(currentPosition == 3) currentPosition=0
+        banner.setCurrentItem(currentPosition,true)
+        currentPosition+=1
+    }
+
+    //3초 마다 페이지 넘기기
+    inner class PagerRunnable:Runnable{
+        override fun run() {
+            while(true){
+                Thread.sleep(2500)
+                handler.sendEmptyMessage(0)
+            }
+        }
     }
 
     // 뷰 페이저에 들어갈 아이템
     private fun getBannerList(): ArrayList<Int> {
         return arrayListOf<Int>(R.drawable.banner_1, R.drawable.banner_2, R.drawable.banner_3)
+    }
+
+    /* 공식문서에 있는 코드 긁어온거임 */
+    inner class ZoomOutPageTransformer : ViewPager2.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            view.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> { // [-Infinity,-1)
+                        // This page is way off-screen to the left.
+                        alpha = 0f
+                    }
+                    position <= 1 -> { // [-1,1]
+                        // Modify the default slide transition to shrink the page as well
+                        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+
+                        // Scale the page down (between MIN_SCALE and 1)
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+
+                        // Fade the page relative to its size.
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> { // (1,+Infinity]
+                        // This page is way off-screen to the right.
+                        alpha = 0f
+                    }
+                }
+            }
+        }
     }
 }
