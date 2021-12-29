d6d951cff1c22936770f65232a5535d7f5a4eeb1
yunpeng <wha02068@gmail.com>
2021-12-29 11:25:04 +0900

    마라톤 로티파일, 하트 토스트

--- app/src/main/java/fragments/HomeFragment.kt
+++ app/src/main/java/fragments/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -95,6 +96,11 @@ class HomeFragment: Fragment() {
         map.setOnClickListener{
             val  myIntent = Intent(context, MapActivity::class.java)
             startActivity(myIntent)
+        }
+
+        //heart
+        heart.setOnClickListener {
+            Toast.makeText(context, "JESUS LOVES YOU", Toast.LENGTH_SHORT).show()
         }
     }
 
